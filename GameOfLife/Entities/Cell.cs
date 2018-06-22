3d358c0c8c1c7654109caa1ba0ad742e03c984d7
cschwarzbeck <cschwarzbeck@TECHNIK-CON>
2018-06-22 16:01:59 +0200

    Moved CellExtensions to Helpers

--- GameOfLife/Entities/Cell.cs
+++ GameOfLife/Entities/Cell.cs
@@ -11,22 +11,4 @@
     {
         Carnivore, Herbivore
     }
-
-    public static class CellExtensions
-    {
-        public static Cell ToAlive(this Cell cell)
-        {
-            return new Cell {IsAlive = true, LifeTime = 1, Diet = cell.Diet};
-        }
-
-        public static Cell Kill(this Cell cell)
-        {
-            return new Cell {IsAlive = false, LifeTime = 0, Diet = cell.Diet};
-        }
-
-        public static Cell IncrementLifetime(this Cell cell)
-        {
-            return new Cell {IsAlive = cell.IsAlive, LifeTime = cell.LifeTime + 1, Diet = cell.Diet};
-        }
-    }
 }
---
+++ GameOfLife/Helpers/CellExtensions.cs
@@ -0,0 +1,22 @@
+﻿using GameOfLife.Entities;
+
+namespace GameOfLife.Helpers
+{
+    public static class CellExtensions
+    {
+        public static Cell ToAlive(this Cell cell)
+        {
+            return new Cell { IsAlive = true, LifeTime = 1, Diet = cell.Diet };
+        }
+
+        public static Cell Kill(this Cell cell)
+        {
+            return new Cell { IsAlive = false, LifeTime = 0, Diet = cell.Diet };
+        }
+
+        public static Cell IncrementLifetime(this Cell cell)
+        {
+            return new Cell { IsAlive = cell.IsAlive, LifeTime = cell.LifeTime + 1, Diet = cell.Diet };
+        }
+    }
+}
